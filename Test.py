d3326913ae2530a89c0e2f7aa972e4d6e29365fc
Wang <wang.kui@CIENET-PB.com>
2019-06-14 15:58:34 +0800

    Company

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-trainLen = 100
+trainLen = 1000
 trainX = np.random.randn(trainLen,1)
 trainY = [i**2 for i in trainX]
 training_data = list(zip(trainX,trainY))
@@ -15,25 +15,37 @@ testX = np.random.randn(testLen,1)
 testY = [i**2 for i in testX]
 testData = list(zip(testX,testY))
 
-initNet = [1,1,1]
+initNet = [1,5,1]
 net = BPNetwork.network(initNet)
-cycle = 10
+cycle = 1
 numPerCycle = 10
 learnRate = 3
 net.fit(training_data,cycle,numPerCycle,learnRate)
 
-
 len = len(net.weights)
 i = [t for t in range(len)]
+
 
 plt.plot(i,net.weights)
 plt.plot(i,net.biases)
 plt.figure();
 
-
-y = net.test(testData)
-plt.scatter(testX,list(y),marker='*')
-plt.scatter(testX,testY,marker='o')
-plt.scatter(trainX,trainY,marker='<')
+reY = net.test(testData)
+plt.scatter(trainX,trainY,marker='*')
+plt.scatter(testX,testY,marker='<')
+plt.scatter(testX,reY,marker='o')
 plt.show()
 
+
+'''
+trX,trY = [vx,vy for (vx,vy) in zip(trainX,trainY)]
+plt.scatter(trX,trY,marker='o')
+
+teX,teY = [vx,vy for (vx,vy) in zip(testX,testY)]
+plt.scatter(teX,teY,marker='o')
+
+plt.scatter(teX,reY,marker='o')
+
+plt.scatter(trainX,trainY,marker='<')
+plt.show()
+'''
